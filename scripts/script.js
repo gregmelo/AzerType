105334000ced324f475d8fc0442ee5d88b7751ab
gregmelo <gregoryvericel6@gmail.com>
2024-05-23 10:14:20 +0200

    Ajout des vérification pour le nom et l'email lors du partage

--- scripts/script.js
+++ scripts/script.js
@@ -18,9 +18,40 @@ function afficherResultat(score, nbMotsProposes) {
   spanScore.innerText = affichageScore;
 }
 
+/**
+ * Cette fonction affiche une proposition, que le joueur devra recopier,
+ * dans la zone "zoneProposition"
+ * @param {string} proposition : la proposition à afficher
+ */
 function afficherProposition(proposition) {
   let zoneProposition = document.querySelector(".zoneProposition");
   zoneProposition.innerText = proposition;
+}
+
+/**
+ * Cette fonction construit et affiche l'email.
+ * @param {string} nom : le nom du joueur
+ * @param {string} email : l'email de la personne avec qui il veut partager son score
+ * @param {string} score : le score.
+ */
+function afficherEmail(nom, email, score) {
+  let mailto = `mailto:${email}?subject=Partage du score Azertype&body=Salut, je suis ${nom} et je viens de réaliser le score ${score} sur le site d'Azertype !`;
+  location.href = mailto;
+}
+
+function validerNom(nom) {
+  if (nom.length >= 2) {
+    return true;
+  }
+  return false;
+}
+
+function validerEmail(email) {
+  let emailRegExp = new RegExp("[a-z0-9.-_]+@[a-z0-9.-_]+\\.[a-z0-9.-_]+");
+  if (emailRegExp.test(email)) {
+    return true;
+  }
+  return false;
 }
 
 /**
@@ -29,15 +60,17 @@ function afficherProposition(proposition) {
  */
 function lancerJeu() {
   // Initialisations
+  initAddEventListenerPopup();
   let score = 0;
-
   let i = 0;
-
   let listeProposition = listeMots;
 
   let btnValiderMot = document.getElementById("btnValiderMot");
   let inputEcriture = document.getElementById("inputEcriture");
+
   afficherProposition(listeProposition[i]);
+
+  // Gestion de l'événement click sur le bouton "valider"
   btnValiderMot.addEventListener("click", () => {
     if (inputEcriture.value === listeProposition[i]) {
       score++;
@@ -53,17 +86,38 @@ function lancerJeu() {
     }
   });
 
+  // Gestion de l'événement change sur les boutons radios.
   let listeBtnRadio = document.querySelectorAll(".optionSource input");
   for (let index = 0; index < listeBtnRadio.length; index++) {
     listeBtnRadio[index].addEventListener("change", (event) => {
-      console.log(event.target.value);
+      // Si c'est le premier élément qui a été modifié, alors nous voulons
+      // jouer avec la listeMots.
       if (event.target.value === "1") {
         listeProposition = listeMots;
       } else {
+        // Sinon nous voulons jouer avec la liste des phrases
         listeProposition = listePhrases;
       }
+      // Et on modifie l'affichage en direct.
       afficherProposition(listeProposition[i]);
     });
   }
+
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let baliseNom = document.getElementById("nom");
+    let nom = baliseNom.value;
+    let baliseMail = document.getElementById("email");
+    let email = baliseMail.value;
+
+    if (validerNom(nom) && validerEmail(email)) {
+      let scoreEmail = `${score} / ${i}`;
+      afficherEmail(nom, email, scoreEmail);
+    } else {
+      console.log("erreur");
+    }
+  });
+
   afficherResultat(score, i);
 }
